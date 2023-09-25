89c18e2531d1405ba087d0f574de62a5e8492b7d
Srujana Golconda <48400536+Sruj13@users.noreply.github.com>
2023-09-25 17:31:23 -0500

    Time: 0 ms (100.00%), Space: 40.5 MB (96.65%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeElement(int[] arr, int val) {
         
-        int j =0;
+       /* int j =0;
         int c=0;
         for(int i=0;i<arr.length;i++)
         {
@@ -11,7 +11,20 @@ class Solution {
                 j++;
             }
         }
-        return j;
+        return j;*/
+        
+        int i = 0;
+        int n = arr.length;
+        while(i<n){
+            if(arr[i]==val){
+                arr[i]=arr[n-1];
+                n--;
+            }
+            else{
+                i++;
+            }
+        }
+        return n;
         
     }
 }

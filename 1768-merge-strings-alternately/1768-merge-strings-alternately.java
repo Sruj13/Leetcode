4b7c5c6b7a956c3c2a04c4455752a8439b9e2275
Srujana Golconda <48400536+Sruj13@users.noreply.github.com>
2023-12-09 20:19:48 -0600

    Time: 7 ms (24.42%), Space: 42.4 MB (5.89%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -5,10 +5,10 @@ class Solution {
         String res="";
         
         for(int i=0;i<max;i++){
-            if(i<word1.length() && Character.isAlphabetic(word1.charAt(i))){
+            if(i<word1.length()){
                 res+=word1.charAt(i);
             }
-            if(i<word2.length() && Character.isAlphabetic(word2.charAt(i))){
+            if(i<word2.length()){
                 res+=word2.charAt(i);
             }
         }

461297ad9a24b7a255f823f6d704a4e544b6885b
Srujana Golconda <48400536+Sruj13@users.noreply.github.com>
2024-02-26 18:17:33 -0600

    Time: 12 ms (97.75%), Space: 49.1 MB (88.32%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,53 @@
+class MyHashSet {
+    
+    boolean[][] h;
+    public MyHashSet() {
+        h = new boolean[1000][];
+        
+    }
+    
+    private int getPrimaryIndex(int key){
+        return key%1000;
+    }
+    private int getSecondaryIndex(int key){
+        return key/1000;
+    }
+    
+    public void add(int key) {
+        int index = getPrimaryIndex(key);
+        int sindex = getSecondaryIndex(key);
+        if(h[index]==null){
+            if(index==0){
+                h[index]= new boolean[1000+1];
+            }
+            else{
+                h[index]=new boolean[1000];
+            }
+        }
+        h[index][sindex] = true;
+    }
+    
+    public void remove(int key) {
+        int index = getPrimaryIndex(key);
+        int sindex = getSecondaryIndex(key);
+        if(h[index]!=null) 
+            h[index][sindex] = false;
+        
+    }
+    
+    public boolean contains(int key) {
+        int index = getPrimaryIndex(key);
+        int sindex = getSecondaryIndex(key);
+        if(h[index]==null)
+            return false;
+        return h[index][sindex];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
